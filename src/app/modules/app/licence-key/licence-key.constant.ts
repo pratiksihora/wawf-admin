1f7bbfeb3cd56a861879f90cc47b34803736db0e
Ruchit <ruchit.ghadiya@saeculumsolutions.com>
2022-12-21 17:37:41 +0530

    table change add

--- src/app/modules/app/licence-key/licence-key.constant.ts
+++ src/app/modules/app/licence-key/licence-key.constant.ts
@@ -15,28 +15,30 @@ import { TableUtil } from "src/app/shared/_core/utils/table"
 export const configureTable = (translate: any, permission: any, data: any): TableConfig => {
   return TableUtil.configure({
     type: 'lazy',
-    dataKey: 'ub_id',
+    dataKey: 'sk_id',
     columns: [
       {
-        field: 'sk_licence_key', header: 'License Key'
+        field: 'sk_licence_key', header: 'License Key',
       },
       {
-        field: 'number_devices', header: 'Number of Devices'
+        field: 'number_devices', header: 'Number of Devices', prepareColumn: (data) => `${data.number_devices || '-'}`,
       },
       {
         field: 'sk_no_of_month', header: 'Number of Month'
       },
       {
-        field: 'sk_created_at', header: 'Created Date'
+        field: 'sk_created_at', header: 'Created Date', type: 'date'
       },
       {
-        field: 'sk_start_date', header: 'Used Date'
+        field: 'sk_start_date', header: 'Used Date', type: 'date', prepareColumn: (data) => `${data.sk_start_date || '-'}`,
       },
       {
-        field: 'executed_date', header: 'Executed Date'
+        field: 'sk_end_date', header: 'Executed Date', type: 'date', prepareColumn: (data) => `${data.sk_end_date || '-'}`,
       },
       {
-        field: 'sk_status', header: 'Current Status'
+        field: 'sk_status', header: 'Current Status', type: 'status', prepareColumn: (data) => ({
+          status: data.sk_status, color: UserRoleColor[data.sk_status],
+        })
       },
     ],
     tableOptions: {
@@ -60,7 +62,7 @@ export const configureTable = (translate: any, permission: any, data: any): Tabl
       iconOnly: true,
       button: { tooltip: 'Extend' },
       iconClass: "svg-icon svg-icon-gray-600 svg-icon-6",
-      iconSVG: 'assets/media/inline-svg/gallery/delete.svg',
+      iconSVG: './assets/media/svg/new-svg-icons/credit-history.svg',
       // confirmation: { type: 'success', title: 'Campaign', message: 'Are you sure you want to Clone this campaign?', confirmation: { title: 'Campaign', message: 'Are you sure you want to Clone this campaign?', button1: true, button1Text: 'Clone', button2: true, button2Text: 'Cancel', button1Class: 'btn btn-success', icon: 'bi bi-back me-3' } }
     },
     devise: true,
@@ -73,7 +75,7 @@ export const configureTable = (translate: any, permission: any, data: any): Tabl
       iconOnly: true,
       button: { tooltip: 'Device History' },
       iconClass: "svg-icon svg-icon-gray-600 svg-icon-6",
-      iconSVG: 'assets/media/inline-svg/gallery/delete.svg',
+      iconSVG: './assets/media/svg/new-svg-icons/credit-history.svg',
       // confirmation: { type: 'success', title: 'Campaign', message: 'Are you sure you want to Clone this campaign?', confirmation: { title: 'Campaign', message: 'Are you sure you want to Clone this campaign?', button1: true, button1Text: 'Clone', button2: true, button2Text: 'Cancel', button1Class: 'btn btn-success', icon: 'bi bi-back me-3' } }
     },
     credit: true,
@@ -86,7 +88,7 @@ export const configureTable = (translate: any, permission: any, data: any): Tabl
       iconOnly: true,
       button: { tooltip: 'Credit History' },
       iconClass: "svg-icon svg-icon-gray-600 svg-icon-6",
-      iconSVG: 'assets/media/inline-svg/gallery/delete.svg',
+      iconSVG: './assets/media/svg/new-svg-icons/credit-history.svg',
       // confirmation: { type: 'success', title: 'Campaign', message: 'Are you sure you want to Clone this campaign?', confirmation: { title: 'Campaign', message: 'Are you sure you want to Clone this campaign?', button1: true, button1Text: 'Clone', button2: true, button2Text: 'Cancel', button1Class: 'btn btn-success', icon: 'bi bi-back me-3' } }
     },
   }, translate, permission, data)
