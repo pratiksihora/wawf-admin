6d365217cf8f7089594df09f0654e8c0dc06cf5d
niravpatel19 <nirav.patel@saeculumsolutions.com>
2022-12-21 20:26:43 +0530

    12 month added

--- src/app/modules/app/licence-key/components/create-licence-key/create-licence-key.constant.ts
+++ src/app/modules/app/licence-key/components/create-licence-key/create-licence-key.constant.ts
@@ -8,7 +8,15 @@ import { ApiUtil } from "src/app/shared/_core/utils/api"
 import { InputUtil, SelectUtil } from "src/app/shared/_core/utils/form/field"
 
 export const FormFields: { [key: string]: Field } = {
-    no_of_month: SelectUtil.configureSelect({ key: 'no_of_month', label: 'Time', smallControl: true, layout: LayoutType.VERTICAL, }, { required: true }, { options: [{ value: 1, text: '1 Month' }, { value: 2, text: '2 Month' }, { value: 3, text: '3 Month' }, { value: 4, text: '4 Month' }, { value: 5, text: '5 Month' }] }, { classNames: { label: 'fw-bold pb-4 fs-6 col-form-label' } }),
+    no_of_month: SelectUtil.configureSelect({ key: 'no_of_month', label: 'Time', smallControl: true, layout: LayoutType.VERTICAL, }, { required: true }, {
+        options: [
+            { value: 1, text: '1 Month' }, { value: 2, text: '2 Month' }, { value: 3, text: '3 Month' },
+            { value: 4, text: '4 Month' }, { value: 5, text: '5 Month' }, { value: 6, text: '6 Month' },
+            { value: 4, text: '4 Month' }, { value: 5, text: '5 Month' }, { value: 6, text: '6 Month' },
+            { value: 7, text: '7 Month' }, { value: 8, text: '8 Month' }, { value: 9, text: '9 Month' },
+            { value: 10, text: '10 Month' }, { value: 11, text: '11 Month' }, { value: 12, text: '12 Month' },
+        ]
+    }, { classNames: { label: 'fw-bold pb-4 fs-6 col-form-label' } }),
     no_of_device: InputUtil.configureNumber({ key: 'no_of_device', label: 'Number Of Devices', placeholder: 'Enter Value', smallControl: true, layout: LayoutType.VERTICAL }, { required: true, type: ValidationType.MAX_100 }, {}, { classNames: { label: 'fw-bold pb-4 fs-6 col-form-label' } }),
     plan_type: SelectUtil.configureSelect({ key: 'plan_type', label: 'Plan Type', smallControl: true, layout: LayoutType.VERTICAL, }, { required: true }, { options: [{ value: 'Premium', text: 'Platinum' }] }, { classNames: { label: 'fw-bold pb-4 fs-6 col-form-label' } }),
 }
--- src/app/modules/app/licence-key/components/extend/extend.constant.ts
+++ src/app/modules/app/licence-key/components/extend/extend.constant.ts
@@ -9,7 +9,15 @@ import { InputUtil, SelectUtil } from "src/app/shared/_core/utils/form/field";
 
 const others = { classNames: { label: 'fw-bold fs-6 col-form-label pb-2' } };
 export const FormFields: { [key: string]: Field } = {
-    sk_no_of_month: SelectUtil.configureSelect({ key: 'sk_no_of_month', label: 'Time to Extend', smallControl: true, unClear: true, layout: LayoutType.VERTICAL, }, { required: true }, { options: [{ value: 1, text: '1 Month' }, { value: 2, text: '2 Month' }, { value: 3, text: '3 Month' }, { value: 4, text: '4 Month' }, { value: 5, text: '5 Month' }] }, others),
+    sk_no_of_month: SelectUtil.configureSelect({ key: 'sk_no_of_month', label: 'Time to Extend', smallControl: true, unClear: true, layout: LayoutType.VERTICAL, }, { required: true }, {
+        options: [
+            { value: 1, text: '1 Month' }, { value: 2, text: '2 Month' }, { value: 3, text: '3 Month' },
+            { value: 4, text: '4 Month' }, { value: 5, text: '5 Month' }, { value: 6, text: '6 Month' },
+            { value: 4, text: '4 Month' }, { value: 5, text: '5 Month' }, { value: 6, text: '6 Month' },
+            { value: 7, text: '7 Month' }, { value: 8, text: '8 Month' }, { value: 9, text: '9 Month' },
+            { value: 10, text: '10 Month' }, { value: 11, text: '11 Month' }, { value: 12, text: '12 Month' },
+        ]
+    }, others),
     sk_no_of_login: InputUtil.configureNumber({
         key: 'sk_no_of_login', label: 'Number Of Device', disabled: true, smallControl: true,
         placeholder: 'e.g. 10', layout: LayoutType.VERTICAL
