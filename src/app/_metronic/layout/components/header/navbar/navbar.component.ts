a33f9adba8d3ca67e8c55b1c9709df68570d3f8e
niravpatel19 <nirav.patel@saeculumsolutions.com>
2022-12-22 18:49:44 +0530

    download extenstion and changes password added.

--- src/app/_metronic/layout/components/header/navbar/navbar.component.ts
+++ src/app/_metronic/layout/components/header/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+//Utills
+import { TokenUtil } from 'src/app/shared/_core/utils/token';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -9,13 +12,21 @@ export class NavbarComponent implements OnInit {
   @Input() appHeaderDefaulMenuDisplay: boolean;
   @Input() isRtl: boolean;
 
+  userData;
+
   itemClass: string = 'ms-1 ms-lg-3';
   btnClass: string =
     'btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px';
   userAvatarClass: string = 'symbol-35px symbol-md-40px';
   btnIconClass: string = 'svg-icon-1';
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userData = TokenUtil.getUser();
+  }
+
+  downloadExtensions() {
+    window.open(this.userData?.reseller_ext_link, '_blank');
+  }
 }
