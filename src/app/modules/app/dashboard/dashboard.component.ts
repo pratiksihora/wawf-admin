3215d7471f7f615f6502aa4aaf0af4a77632342d
Ruchit <ruchit.ghadiya@saeculumsolutions.com>
2022-12-22 16:02:56 +0530

    dashboard page api  bind

--- src/app/modules/app/dashboard/dashboard.component.ts
+++ src/app/modules/app/dashboard/dashboard.component.ts
@@ -1,4 +1,15 @@
-import { Component, ViewChild, HostBinding } from '@angular/core';
+import { Component, ViewChild, HostBinding, OnInit, ChangeDetectorRef } from '@angular/core';
+
+//service
+import { TableService } from 'src/app/api/services/common/table/table.service';
+
+//enum & interface
+import { ApiAction } from 'src/app/shared/constants/models/api';
+import { ApiModule } from 'src/app/api/enums/api-module.enum';
+
+//utils
+import { ApiUtil } from 'src/app/shared/_core/utils/api';
+
 import { ModalConfig, ModalComponent } from '../../../_metronic/partials';
 
 @Component({
@@ -6,7 +17,7 @@ import { ModalConfig, ModalComponent } from '../../../_metronic/partials';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   @HostBinding('class') class = 'flex-fill';
   modalConfig: ModalConfig = {
     modalTitle: 'Modal title',
@@ -14,9 +25,25 @@ export class DashboardComponent {
     closeButtonLabel: 'Cancel'
   };
   @ViewChild('modal') private modalComponent: ModalComponent;
-  constructor() { }
+  getData: any;
+  constructor(public tableService: TableService, public cdr: ChangeDetectorRef,) { }
+
+  ngOnInit(): void {
+    this.getAllCount();
+  }
 
   async openModal() {
     return await this.modalComponent.open();
   }
+
+  getAllCount() {
+    const common: ApiAction = ApiUtil.configureGet({ module: ApiModule.API, url: '/v1/reseller/dashboard', title: 'User' })
+    this.tableService.tableCommon(common, {}).subscribe({
+      next: (res) => {
+        this.getData = res.data;
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
 }
